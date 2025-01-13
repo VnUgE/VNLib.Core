4219e6527902350cda05ceb23d3624ceafcb6323
vnugent <public@vaughnnugent.com>
2025-01-13 00:37:48 -0500

    fix target framework variable, and extend userencoded data api

--- lib/Plugins.Essentials/src/Users/UserEncodedData.cs
+++ lib/Plugins.Essentials/src/Users/UserEncodedData.cs
@@ -1,5 +1,5 @@
 ﻿/*
-* Copyright (c) 2024 Vaughn Nugent
+* Copyright (c) 2025 Vaughn Nugent
 * 
 * Library: VNLib
 * Package: VNLib.Plugins.Essentials
@@ -39,20 +39,14 @@ namespace VNLib.Plugins.Essentials.Users
     public static class UserEncodedData
     {
         /// <summary>
-        /// Recovers encoded items from the user's account object
+        /// Decodes a base64url encoded string into an object of type T
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="store">The data store to read encoded data from</param>
-        /// <param name="index">The property index in the user fields to recover the objects from</param>
-        /// <returns>The encoded properties from the desired user index</returns>
-        public static T? Decode<T>(IIndexable<string, string> store, string index) where T : class
+        /// <typeparam name="T">The desired stored object type</typeparam>
+        /// <param name="encodedData">The previously encoded string data to decode</param>
+        /// <returns>The decoded object instance if data was successfully recovered</returns>
+        public static T? Decode<T>(ReadOnlySpan<char> encodedData) where T : class
         {
-            ArgumentNullException.ThrowIfNull(store);
-            ArgumentException.ThrowIfNullOrWhiteSpace(index);
-
-            string? encodedData = store[index];
-
-            if (string.IsNullOrWhiteSpace(encodedData))
+            if (encodedData.IsEmpty)
             {
                 return null;
             }
@@ -75,6 +69,41 @@ namespace VNLib.Plugins.Essentials.Users
         }
 
         /// <summary>
+        /// Recovers encoded items from the user's account object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="store">The data store to read encoded data from</param>
+        /// <param name="index">The property index in the user fields to recover the objects from</param>
+        /// <returns>The encoded properties from the desired user index</returns>
+        public static T? Decode<T>(IIndexable<string, string> store, string index) where T : class
+        {
+            ArgumentNullException.ThrowIfNull(store);
+            ArgumentException.ThrowIfNullOrWhiteSpace(index);
+
+            return Decode<T>(store[index]);
+        }      
+
+        /// <summary>
+        /// Writes a set of items to the user's account object, encoded in base64
+        /// </summary>
+        /// <typeparam name="T">The class instance to encode</typeparam>
+        /// <param name="instance">The object instance to encode and store</param>
+        public static string? Encode<T>(T? instance) where T : class
+        {
+            if (instance is null)
+            {
+                return null;
+            }
+
+            //Use a memory stream to serialize the items safely
+            using VnMemoryStream ms = new(MemoryUtil.Shared, bufferSize: 1024, zero: false);
+
+            JsonSerializer.Serialize(ms, instance, Statics.SR_OPTIONS);
+
+            return VnEncoding.Base64UrlEncode(ms.AsSpan(), includePadding: false);
+        }
+
+        /// <summary>
         /// Writes a set of items to the user's account object, encoded in base64
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -86,18 +115,7 @@ namespace VNLib.Plugins.Essentials.Users
             ArgumentNullException.ThrowIfNull(store);
             ArgumentException.ThrowIfNullOrWhiteSpace(index);
 
-            if (instance == null)
-            {
-                store[index] = null!;
-                return;
-            }
-
-            //Use a memory stream to serialize the items safely
-            using VnMemoryStream ms = new(MemoryUtil.Shared, bufferSize: 1024, zero: false);
-
-            JsonSerializer.Serialize(ms, instance, Statics.SR_OPTIONS);
-
-            store[index] = VnEncoding.Base64UrlEncode(ms.AsSpan(), includePadding: false);
+            store[index] = Encode(instance)!;
         }
     }
 }
