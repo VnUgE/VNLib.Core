4dcf08abb2868f015195b421eb53364aac35e18c
vnugent <public@vaughnnugent.com>
2025-02-07 14:51:58 -0500

    fix copyright

--- lib/Utils.Cryptography/monocypher/src/platform.h
+++ lib/Utils.Cryptography/monocypher/src/platform.h
@@ -5,18 +5,18 @@
 * Package: vnlib_monocypher
 * File: platform.h
 *
-* vnlib_compress is free software: you can redistribute it and/or modify
+* vnlib_monocypher is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published
 * by the Free Software Foundation, either version 2 of the License,
 * or (at your option) any later version.
 *
-* vnlib_compress is distributed in the hope that it will be useful,
+* vnlib_monocypher is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 * General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with vnlib_compress. If not, see http://www.gnu.org/licenses/.
+* along with vnlib_monocypher. If not, see http://www.gnu.org/licenses/.
 */
 
 
