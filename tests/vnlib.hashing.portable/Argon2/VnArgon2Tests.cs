f75283416a960cafdd505bf21706c7519cc3da04
vnugent <public@vaughnnugent.com>
2025-02-07 15:28:57 -0500

    [test] fix hash tests with substituted parameters

--- tests/vnlib.hashing.portable/Argon2/VnArgon2Tests.cs
+++ tests/vnlib.hashing.portable/Argon2/VnArgon2Tests.cs
@@ -19,7 +19,7 @@ namespace VNLib.Hashing.Tests
         [TestMethod]
         public void Argon2IdHashTest()
         {
-            const string NativeArgon2KnownHash = "5rE9QJP1nqQXlLDSxjj7PwVv7oub33lP+zxvaRjcM4ApKPtirF5N5V7dH55MYqwNv/e7qh+mqQWT2u/OR4wU3w==";
+            const string NativeArgon2KnownHash = "VqnpGP/WhJou62BB4Lcunaih5+l4wp2kFzL2XdDo8fZ898u/SizOj008MdxPB0uAGy9Ed5UBkqp8XyZwzS8kfQ==";
 
             TestArgon2Lib(VnArgon2.GetOrLoadSharedLib(), NativeArgon2KnownHash);
         }
@@ -34,7 +34,7 @@ namespace VNLib.Hashing.Tests
              * So they have to be compared separately.
              */
 
-            const string McKnownHash = "V7Xfd2i6lt3ZfWhrpN+6NCH9o2SZ1lJ/roqchm9uGU+XCizqS6ZAOs6am97tw/WGpWzQMKmE/F3Yp/2kPMavkA==";
+            const string McKnownHash = "x5G97+BiK04sZ9lUh+BI/jeTzxzESrCbXX7gKKHaqm0UF6FIEuIJAX5lPAF8+uhnbGomZXuOrXDCxyIRMjubuA==";
 
             Assert.IsTrue(MonoCypherLibrary.CanLoadDefaultLibrary());
 
@@ -50,20 +50,20 @@ namespace VNLib.Hashing.Tests
             const string SaltBase64 = "USixgneVaOQYhlglhfLr9A==";            
             const uint HashSize = 64u;
 
-            /*
-             * The output of vnlib-argon2 is slightly different from the native library
-             * as it encodes the salt as base64 as an s= paremter in the parameter
-             * list instead of prepending to the hash with a $ delimiter. It also
-             * uses standard base64 encoding instead of url-safe base64 encoding.
-             */
-            string KnownOutput = $"$argon2id$v=19,m=65536,t=2,p=4,s={SaltBase64}${knownHash}";
-
             Argon2CostParams a2Params = new()
             {
                 MemoryCost = 65536,
                 Parallelism = 4,        // MC ignores this value, sort of.
                 TimeCost = 3,           // >=3 is recommended
             };
+
+            /*
+             * The output of vnlib-argon2 is slightly different from the native library
+             * as it encodes the salt as base64 as an s= paremter in the parameter
+             * list instead of prepending to the hash with a $ delimiter. It also
+             * uses standard base64 encoding instead of url-safe base64 encoding.
+             */
+            string KnownOutput = $"$argon2id$v=19,m={a2Params.MemoryCost},t={a2Params.TimeCost},p={a2Params.Parallelism},s={SaltBase64}${knownHash}";
 
             //Check that empty secret is allowed
             string passHash = library.Hash2id(
