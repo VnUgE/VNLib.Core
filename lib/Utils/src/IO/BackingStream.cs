c822a87785f330a7b0b9cfb14c30fb8ad369bcda
vnugent <public@vaughnnugent.com>
2025-07-24 11:41:30 -0400

    chore(utils)!: enable warnings as errors for vnlib.utils & update backingstream
    BREAKING CHANGE: BackingStream class was updated to fix the remaining compiler warnings for utils so warnings as errors could be enabled

--- lib/Utils/src/IO/BackingStream.cs
+++ lib/Utils/src/IO/BackingStream.cs
@@ -1,5 +1,5 @@
 ﻿/*
-* Copyright (c) 2022 Vaughn Nugent
+* Copyright (c) 2025 Vaughn Nugent
 * 
 * Library: VNLib
 * Package: VNLib.Utils
@@ -39,65 +39,80 @@ namespace VNLib.Utils.IO
         /// <summary>
         /// The backing/underlying stream operations are being performed on
         /// </summary>
-        protected T BaseStream { get; set; }
+        protected abstract T BaseStream { get; }
+
         /// <summary>
         /// A value that will cause all calls to write to throw <see cref="NotSupportedException"/>
         /// </summary>
         protected bool ForceReadOnly { get; set; }
+
         ///<inheritdoc/>
         public override bool CanRead => BaseStream.CanRead;
+
         ///<inheritdoc/>
         public override bool CanSeek => BaseStream.CanSeek;
+
         ///<inheritdoc/>
         public override bool CanWrite => BaseStream.CanWrite && !ForceReadOnly;
+
         ///<inheritdoc/>
         public override long Length => BaseStream.Length;
+
         ///<inheritdoc/>
         public override int WriteTimeout { get => BaseStream.WriteTimeout; set => BaseStream.WriteTimeout = value; }
+
         ///<inheritdoc/>
         public override int ReadTimeout { get => BaseStream.ReadTimeout; set => BaseStream.ReadTimeout = value; }
+
         ///<inheritdoc/>
         public override long Position { get => BaseStream.Position; set => BaseStream.Position = value; }
+
         ///<inheritdoc/>
         public override void Flush()
         {
             BaseStream.Flush();
             OnFlush();
         }
+
         ///<inheritdoc/>
         public override int Read(byte[] buffer, int offset, int count) => BaseStream.Read(buffer, offset, count);
+
         ///<inheritdoc/>
         public override int Read(Span<byte> buffer) => BaseStream.Read(buffer);
+
         ///<inheritdoc/>
         public override long Seek(long offset, SeekOrigin origin) => BaseStream.Seek(offset, origin);
+
         ///<inheritdoc/>
         public override void SetLength(long value) => BaseStream.SetLength(value);
+
         ///<inheritdoc/>
         public override void Write(byte[] buffer, int offset, int count) 
         {
-            if (ForceReadOnly)
-            {
-                throw new NotSupportedException("Stream is set to readonly mode");
-            }
+            ThrowIfReadonly();
+
             BaseStream.Write(buffer, offset, count);
+            
             //Call onwrite function
             OnWrite(count);
         }
+
         ///<inheritdoc/>
         public override void Write(ReadOnlySpan<byte> buffer)
         {
-            if (ForceReadOnly)
-            {
-                throw new NotSupportedException("Stream is set to readonly mode");
-            }
+            ThrowIfReadonly();
+
             BaseStream.Write(buffer);
+            
             //Call onwrite function
             OnWrite(buffer.Length);
         }
+
         ///<inheritdoc/>
         public override void Close()
         {
             BaseStream.Close();
+            
             //Call on close function
             OnClose();
         }
@@ -106,40 +121,39 @@ namespace VNLib.Utils.IO
         /// Raised directly after the base stream is closed, when a call to close is made
         /// </summary>
         protected virtual void OnClose() { }
+        
         /// <summary>
         /// Raised directly after the base stream is flushed, when a call to flush is made
         /// </summary>
         protected virtual void OnFlush() { }
+        
         /// <summary>
-        /// Raised directly after a successfull write operation.
+        /// Raised directly after a successful write operation.
         /// </summary>
         /// <param name="count">The number of bytes written to the stream</param>
         protected virtual void OnWrite(int count) { }
 
         ///<inheritdoc/>
-        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-        {
-            return BaseStream.ReadAsync(buffer, offset, count, cancellationToken);
-        }
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken) 
+            => BaseStream.ReadAsync(buffer, offset, count, cancellationToken);
+
         ///<inheritdoc/>
-        public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
-        {
-            return BaseStream.ReadAsync(buffer, cancellationToken);
-        }
+        public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default) 
+            => BaseStream.ReadAsync(buffer, cancellationToken);
+
         ///<inheritdoc/>
-        public override void CopyTo(Stream destination, int bufferSize) => BaseStream.CopyTo(destination, bufferSize);
+        public override void CopyTo(Stream destination, int bufferSize) 
+            => BaseStream.CopyTo(destination, bufferSize);
+
         ///<inheritdoc/>
-        public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
-        {
-            return BaseStream.CopyToAsync(destination, bufferSize, cancellationToken);
-        }
+        public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken) 
+            => BaseStream.CopyToAsync(destination, bufferSize, cancellationToken);
+        
         ///<inheritdoc/>
         public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         {
-            if (ForceReadOnly)
-            {
-                throw new NotSupportedException("Stream is set to readonly mode");
-            }
+            ThrowIfReadonly();
+
             //We want to maintain pass through as much as possible, so supress warning
 #pragma warning disable CA1835 // Prefer the 'Memory'-based overloads for 'ReadAsync' and 'WriteAsync'
             await BaseStream.WriteAsync(buffer, offset, count, cancellationToken);
@@ -148,21 +162,23 @@ namespace VNLib.Utils.IO
             //Call on-write and pass the number of bytes written
             OnWrite(count);
         }
+        
         ///<inheritdoc/>
         public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
         {
-            if (ForceReadOnly)
-            {
-                throw new NotSupportedException("Stream is set to readonly mode");
-            }
+            ThrowIfReadonly();
+
             await BaseStream.WriteAsync(buffer, cancellationToken);
+
             //Call on-write and pass the length
             OnWrite(buffer.Length);
         }
+
         ///<inheritdoc/>
         public override async Task FlushAsync(CancellationToken cancellationToken)
         {
             await BaseStream.FlushAsync(cancellationToken);
+        
             //Call onflush 
             OnFlush();
         }
@@ -172,10 +188,25 @@ namespace VNLib.Utils.IO
         {
             //Dispose the base stream and await it
             await BaseStream.DisposeAsync();
+            
             //Call onclose
             OnClose();
+            
             //Suppress finalize
             GC.SuppressFinalize(this);
-        }      
+        }
+
+
+        /// <summary>
+        /// Throws a <see cref="NotSupportedException"/> if the stream is set to readonly mode  .
+        /// </summary>
+        /// <exception cref="NotSupportedException"></exception>
+        protected void ThrowIfReadonly()
+        {
+            if (ForceReadOnly)
+            {
+                throw new NotSupportedException("Stream is set to readonly mode");
+            }
+        }
     }
 }
