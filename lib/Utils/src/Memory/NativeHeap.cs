04bad0e8516289a5b8159e19c76c4970f290d387
vnugent <public@vaughnnugent.com>
2025-09-08 13:16:12 -0400

    Squashed commit of the following:
    commit 8f0d530afd677c13b74d5bd45ac7e7279dc43268
    Author: vnugent <public@vaughnnugent.com>
    Date:   Mon Sep 8 13:13:48 2025 -0400
    
        chore(changelog): update changelog for rc10
    
    commit 5007793c2f417038661c8837e48ae57d8346e9d2
    Author: vnugent <public@vaughnnugent.com>
    Date:   Mon Sep 8 12:06:30 2025 -0400
    
        chore(deps): Update zstd to `98d2b90`
    
    commit 4b21138978586fbce46427ec65a2f09c7d2526ba
    Author: vnugent <public@vaughnnugent.com>
    Date:   Mon Sep 8 11:57:18 2025 -0400
    
        chore(deps): Update brotli to `98a89b1`
    
    commit fe4298e377fe7f6da39c5ec09c62f8b5731fe9e2
    Author: vnugent <public@vaughnnugent.com>
    Date:   Mon Sep 8 11:38:08 2025 -0400
    
        add internal platform support attribute for the `Access()` function
    
    commit 1fcf6ed12098de524d7e17b782252cd5dd634170
    Author: vnugent <public@vaughnnugent.com>
    Date:   Thu Sep 4 16:10:57 2025 -0400
    
        fix(webserver): Fix custom response headers not getting sent by the server
    
    commit c31478cb2fe85c6034d9f47d958ef4552d100f0a
    Author: vnugent <public@vaughnnugent.com>
    Date:   Wed Sep 3 15:49:47 2025 -0400
    
        feat: Added public function `NativeHeap.CreateHeap` that accepts existing library handles
    
        A public API function was added to the `NativeHeap` class named `CreateHeap` that attempts to create a new `NativeHeap` instance by using features from the specified ELF library handle.
    
    commit 6d34463386237fb156dac105c4ca97c118f0ea2c
    Author: vnugent <public@vaughnnugent.com>
    Date:   Wed Aug 27 15:20:16 2025 -0400
    
        chore(tests): Update MSTest to 3.10.3 for all test framework components
    
    commit 006ce478d69e4d0e9ffa33af1794f81e9749629c
    Merge: c67d690 373635e
    Author: vnugent <public@vaughnnugent.com>
    Date:   Tue Aug 26 15:43:13 2025 -0400
    
        merge branch 'master' into 'develop'

--- lib/Utils/src/Memory/NativeHeap.cs
+++ lib/Utils/src/Memory/NativeHeap.cs
@@ -24,11 +24,11 @@
 
 using System;
 using System.Diagnostics;
-using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-using VNLib.Utils.Native;
 using VNLib.Utils.Extensions;
+using VNLib.Utils.Native;
 
 namespace VNLib.Utils.Memory
 {
@@ -63,17 +63,55 @@ namespace VNLib.Utils.Memory
         /// <summary>
         /// <para>
         /// Loads an unmanaged heap at runtime, into the current process at the given path. The dll must conform
-        /// to the unmanaged heap format. After the method table is loaded, the heapCreate method is called to 
+        /// to the unmanaged heap format. After the method table is loaded, the heapCreate function is called to 
         /// initialze the heap.
         /// </para>
         /// </summary>
         /// <param name="dllPath">The path to the heap's dll file to load into the process.</param>
         /// <param name="searchPath">The native library search path</param>
-        /// <param name="creationFlags">Specifes the creation flags to pass to the heap creaetion method</param>
-        /// <param name="flags">Generic flags passed directly to the heap creation method</param>
+        /// <param name="creationFlags">Specifes the <see cref="HeapCreation"/> flags to pass to the heapCreate function</param>
+        /// <param name="flags">Generic flags passed directly to the heapCreate function</param>
         /// <returns>The newly initialized <see cref="NativeHeap"/></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="DllNotFoundException"></exception>
         public unsafe static NativeHeap LoadHeap(string dllPath, DllImportSearchPath searchPath, HeapCreation creationFlags, ERRNO flags)
         {
+            //Try to load the library
+            SafeLibraryHandle library = SafeLibraryHandle.LoadLibrary(dllPath, searchPath);
+            try
+            {
+                Trace.WriteLine($"Creating user defined native heap at {dllPath}");
+
+                //Create the heap
+                return CreateHeap(
+                    library, 
+                    ownsHandle: true,  // Owns the handle because it was created/loaded here
+                    creationFlags, 
+                    flags
+                );
+            }
+            catch
+            {
+                //Cleanup
+                library.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new heap from a <see cref="SafeLibraryHandle"/> that is known to be a <see cref="NativeHeap"/>
+        /// ELF library and safe to load named symbols from
+        /// </summary>
+        /// <param name="libHandle">The existing <see cref="SafeLibraryHandle"/></param>
+        /// <param name="ownsHandle">A boolen value indicating if the heap owns the lifetime of the handle and should dispose it when disposed</param>
+        /// <param name="creationFlags">Specifes the <see cref="HeapCreation"/> flags to pass to the heapCreate function</param>
+        /// <param name="flags">Generic flags passed directly to the heapCreate function</param>
+        /// <returns>The newly initialized <see cref="NativeHeap"/></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public unsafe static NativeHeap CreateHeap(SafeLibraryHandle libHandle, bool ownsHandle, HeapCreation creationFlags, ERRNO flags)
+        {
+            ArgumentNullException.ThrowIfNull(libHandle);
+
             //Create a flags structure with defaults
             UnmanagedHeapDescriptor hFlags = new()
             {
@@ -82,41 +120,27 @@ namespace VNLib.Utils.Memory
                 HeapPointer     = IntPtr.Zero
             };
 
-            //Create the heap
-            return LoadHeapCore(dllPath, searchPath, &hFlags);
+            return CreateHeap(libHandle, ownsHandle, &hFlags);
         }
 
-        private unsafe static NativeHeap LoadHeapCore(string path, DllImportSearchPath searchPath, UnmanagedHeapDescriptor* flags)
+        private unsafe static NativeHeap CreateHeap(SafeLibraryHandle libHandle, bool ownsHandle, UnmanagedHeapDescriptor* flags)
         {
-            //Try to load the library
-            SafeLibraryHandle library = SafeLibraryHandle.LoadLibrary(path, searchPath);
-            try
+            //Open method table
+            HeapMethods table = new(libHandle, ownsHandle);           
+
+            //Get the create method
+            CreateHeapDelegate create = libHandle.DangerousGetFunction<CreateHeapDelegate>();
+
+            //Create the new heap
+            if (!create(flags))
             {
-                //Open method table
-                HeapMethods table = new(library);
-
-                Trace.WriteLine($"Creating user defined native heap at {path}");
-
-                //Get the create method
-                CreateHeapDelegate create = library.DangerousGetFunction<CreateHeapDelegate>();
-
-                //Create the new heap
-                if (!create(flags))
-                {
-                    throw new NativeMemoryException("Failed to create the new heap, the heap create method returned a null pointer");
-                }
-
-                Trace.WriteLine($"Successfully created user defined native heap 0x{flags->HeapPointer:x} with flags 0x{flags->CreationFlags:x}");
-
-                //Return the neap heap
-                return new(flags, table);
+                throw new NativeMemoryException("Failed to create the new heap, the heap create method returned a null pointer");
             }
-            catch
-            {
-                //Cleanup
-                library.Dispose();
-                throw;
-            }
+
+            Trace.WriteLine($"Successfully created user defined native heap 0x{flags->HeapPointer:x} with flags 0x{flags->CreationFlags:x}");
+
+            //Return the neap heap
+            return new(flags, table);
         }
        
         private HeapMethods MethodTable;
@@ -153,7 +177,10 @@ namespace VNLib.Utils.Memory
             bool ret = MethodTable.Destroy(handle);
 
             //Free the library
-            MethodTable.Library.Dispose();
+            if (MethodTable.OwnsHandle)
+            {
+                MethodTable.Library.Dispose();
+            }
 
             //Cleanup the method table
             MethodTable = default;
@@ -192,7 +219,7 @@ namespace VNLib.Utils.Memory
             public HeapCreation CreationFlags;
         }
 
-        readonly record struct HeapMethods(SafeLibraryHandle Library)
+        readonly record struct HeapMethods(SafeLibraryHandle Library, bool OwnsHandle)
         {
             public readonly AllocDelegate Alloc = Library.DangerousGetFunction<AllocDelegate>();
             public readonly ReallocDelegate Realloc = Library.DangerousGetFunction<ReallocDelegate>();
