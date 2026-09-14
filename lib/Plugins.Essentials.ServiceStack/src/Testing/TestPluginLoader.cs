9f48cd50e53f71963d8a79bfd683265010f84ef9
vnugent <public@vaughnnugent.com>
2026-09-14 15:05:54 -0400

    doc(servicestack): Fix incorrect license headers and copyright dates
    closes #55
    
    [no ci]

--- lib/Plugins.Essentials.ServiceStack/src/Testing/TestPluginLoader.cs
+++ lib/Plugins.Essentials.ServiceStack/src/Testing/TestPluginLoader.cs
@@ -2,24 +2,24 @@
 * Copyright (c) 2026 Vaughn Nugent
 * 
 * Library: VNLib
-* Package: VNLib.Plugins
+* Package: VNLib.Plugins.Essentials.ServiceStack
 * File: TestPluginLoader.cs 
 *
-* TestPluginLoader.cs is part of VNLib.Plugins which is part 
-* of the larger VNLib collection of libraries and utilities.
+* TestPluginLoader.cs is part of VNLib.Plugins.Essentials.ServiceStack which is part of the larger 
+* VNLib collection of libraries and utilities.
 *
-* VNLib.Plugins is free software: you can redistribute it and/or modify 
-* it under the terms of the GNU General Public License as published
-* by the Free Software Foundation, either version 2 of the License,
-* or (at your option) any later version.
+* VNLib.Plugins.Essentials.ServiceStack is free software: you can redistribute it and/or modify 
+* it under the terms of the GNU Affero General Public License as 
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
 *
-* VNLib.Plugins is distributed in the hope that it will be useful,
+* VNLib.Plugins.Essentials.ServiceStack is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-* General Public License for more details.
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
 *
-* You should have received a copy of the GNU General Public License 
-* along with VNLib.Plugins. If not, see http://www.gnu.org/licenses/.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see https://www.gnu.org/licenses/.
 */
 
 //Only export on DEBUG builds for testing purposes
